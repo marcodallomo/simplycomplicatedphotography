27114f04fe67fcf3f413235c53362d20a6063302
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2021-11-22 15:03:57 +0000

    added files

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,29 @@
+// module.exports = {
+//   globDirectory: "public/",
+//   globPatterns: ["**/*.{png,jpg,ico,html,js,json,txt}"],
+//   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+//   swDest: "public/sw.js",
+// };
+
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{png,jpg,ico,html,js,json,txt}'
-	],
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
-	swDest: 'public/sw.js'
+  globDirectory: "public/",
+  globPatterns: ["**/*.{png,jpg,ico,html,js,json,txt}"],
+
+  swDest: "public/sw.js",
+
+  // Define runtime caching rules.
+  runtimeCaching: [
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+      // Apply a cache-first strategy.
+      handler: "CacheFirst",
+
+      options: {
+        // Use a custom cache name.
+        cacheName: "images",
+      },
+    },
+  ],
 };
